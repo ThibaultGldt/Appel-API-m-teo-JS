a025be943f41c2b603914b25bbfce64f4610171d
GLANDUT Thibault <glandutt@hostname.iut2.upmf-grenoble.fr>
2020-10-21 11:36:05 +0200

    test jquery

--- API_feature.js
+++ API_feature.js
@@ -1,10 +1,5 @@
 ///api/location/search/?query=(query)
 $("#btn-lancer-recherche").click(function(){
 
-  $(this).ajax({url:/api/location/search/?query=($(this).val()),
-          dataType: 'json',
-          success: function(result){
-            console.log(result);
-          }
-        });
+  console.log("test");
 })
