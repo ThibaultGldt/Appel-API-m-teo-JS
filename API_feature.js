b4b8536153676ce015498af35e9b0706f0d5a59a
thibault <glandutthibault@gmail.com>
2020-10-29 22:41:02 +0100

    création blocs résultats

--- API_feature.js
+++ API_feature.js
@@ -18,25 +18,21 @@ $("#btn-lancer-recherche").click(function(){
       }
   })*/
 
-  $.getJSON(proxy+'https://www.metaweather.com/api/location/search/?query='+query, function(data){
-    console.log(data);
-    $("#nom_ville").text(data[0].title);
-    $.getJSON(proxy+'https://www.metaweather.com/api/location/'+data[0].woeid, function(rep){
+  $.getJSON(proxy+'https://www.metaweather.com/api/location/search/?query='+query, function(data){//demande de localisation
+    console.log(data);//test
+    $("#nom_ville").text(data[0].title);//test
+
+    $.getJSON(proxy+'https://www.metaweather.com/api/location/'+data[0].woeid, function(rep){//demande infos sur la météo de la localisation
       console.log(rep);
       console.log(rep.consolidated_weather[0]);
       console.log('https://www.metaweather.com/static/img/weather/png/'+rep.consolidated_weather[0].weather_state_abbr+'.png')
+      $.each(rep.consolidated_weather, function(){
+        $("#bloc-resultats").append("<div id=\"bloc-jour\"> <p>"+this.applicable_date+"</p> <p>Icone</p> <p>Température</p> <p>Vent</p> <p>Humidité</p> </div>");
+      })
     })
   })
 })
 
-for(var i = 0; i < 5; i++){
-    $("#bloc-resultats").append("<div id=\"bloc-jour\"> <p>Date</p> <p>Icone</p> <p>Température</p> <p>Vent</p> <p>Humidité</p> </div>");
-}
-
-function appelLocalisationAPI(query){
-
-}
-
-function appel(){
-
-}
+/*for(var i = 0; i < 5; i++){
+    $("#bloc-resultats").append("<div id=\"bloc-jour\"> <p>""</p> <p>Icone</p> <p>Température</p> <p>Vent</p> <p>Humidité</p> </div>");
+}*/
