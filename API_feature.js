f4d761ae3547f1a28119c9c6fb58dee1117ac74f
thibault <glandutthibault@gmail.com>
2020-10-30 11:00:52 +0100

    test design data

--- API_feature.js
+++ API_feature.js
@@ -23,15 +23,15 @@ $("#btn-lancer-recherche").click(function(){
 
     $.getJSON(proxy+'https://www.metaweather.com/api/location/'+data[0].woeid, function(rep){//demande infos sur la météo de la localisation
       $("#localisation").append(rep.parent.title);
+      var i = 1;
       $.each(rep.consolidated_weather, function(){
         var icone = 'https://www.metaweather.com/static/img/weather/png/' + this.weather_state_abbr + '.png';
-        $("#bloc-resultats").append("<div id=\"bloc-jour\">  </div>");//création du bloc contenant toutes les données pour 1 jour
-        $("#bloc-jour").append("<p>"+this.applicable_date+"</p>");
-        $("#bloc-jour").append("<div id=\"temp\" class=\"case\" </div>");//création du bloc contenant les témperatures: Max, Moyenne, Min
-          $("#temp").append("<p id=\"max_temp\">"+this.max_temp+"</p>");
-          $("#temp").append("<p id=\"the_temp\">"+this.the_temp+"</p>");
-          $("#temp").append("<p id=\"min_temp\">"+this.min_temp+"</p>");
-
+        $("#bloc-resultats").append("<div id=\"jour"+i"\"class=\"bloc-jour\">  </div>");//création du bloc contenant toutes les données pour 1 jour
+        $("#bloc-jour"+i).append("<p>"+this.applicable_date+"</p>");
+        $("#bloc-jour"+i).append("<div id=\"temp\" class=\"case\" </div>");//création du bloc contenant les témperatures: Max, Moyenne, Min
+          $("#bloc-jour"+i+" #temp").append("<p id=\"max_temp\">"+this.max_temp+"</p>");
+          $("#bloc-jour"+i+" #temp").append("<p id=\"the_temp\">"+this.the_temp+"</p>");
+          $("#bloc-jour"+i+" #temp").append("<p id=\"min_temp\">"+this.min_temp+"</p>");
       })
     })
   })
