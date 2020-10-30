4c9ec6846e5593fefd6d79b0084f554bdfe13632
thibault <glandutthibault@gmail.com>
2020-10-30 11:04:44 +0100

    test design data

--- API_feature.js
+++ API_feature.js
@@ -26,7 +26,7 @@ $("#btn-lancer-recherche").click(function(){
       var i = 1;
       $.each(rep.consolidated_weather, function(){
         var icone = 'https://www.metaweather.com/static/img/weather/png/' + this.weather_state_abbr + '.png';
-        $("#bloc-resultats").append("<div id=\"jour"+i"\"class=\"bloc-jour\">  </div>");//création du bloc contenant toutes les données pour 1 jour
+        $("#bloc-resultats").append("<div id=\"jour"+i+"\"class=\"bloc-jour\">  </div>");//création du bloc contenant toutes les données pour 1 jour
         $("#bloc-jour"+i).append("<p>"+this.applicable_date+"</p>");
         $("#bloc-jour"+i).append("<div id=\"temp\" class=\"case\" </div>");//création du bloc contenant les témperatures: Max, Moyenne, Min
           $("#bloc-jour"+i+" #temp").append("<p id=\"max_temp\">"+this.max_temp+"</p>");
