cdbfe2e297c4fd838e6b436fac43baa9fc8a218f
thibault <glandutthibault@gmail.com>
2020-10-31 16:45:05 +0100

    test recherche ds localstorage

---
+++ fav_feature.js
@@ -0,0 +1,16 @@
+var proxy = 'https://cors-anywhere.herokuapp.com/';
+var url1 = 'https://www.metaweather.com/api/location/search/?query=';
+
+var cookies;
+
+
+$("#btn-favoris").click(function(){
+  var query = $("#champs_saisi").val();
+  $.getJSON(proxy + url1 + query, function(data){//demande de localisation
+
+    if(data.length != 1){
+        alert("Veuillez entrer un nom de ville valide et complet");
+    }else if($.inArray(query, localStorage)){
+      console.log("ca marche");
+    }
+})
