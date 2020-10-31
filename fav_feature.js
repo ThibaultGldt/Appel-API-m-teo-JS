497072f0f34f60dc0053efceaab783ff8bb341fb
thibault <glandutthibault@gmail.com>
2020-10-31 18:07:51 +0100

    test recherche ds localstorage

--- fav_feature.js
+++ fav_feature.js
@@ -1,13 +1,10 @@
 var proxy = 'https://cors-anywhere.herokuapp.com/';
 var url1 = 'https://www.metaweather.com/api/location/search/?query=';
 
-if(localStorage.length ==0){
+if(localStorage.length ==0) {
   $(".info-vide").last().css("display", "block");
 }else{
-  for(var key in localStorage){
-    $("#liste-favoris").append("<li>\n<span title=\"Cliquer pour relancer la recherche\">"+localStorage.getItem(key)+"</span>\n"
-                                +"<img src=\"images/croix.svg\" alt=\"Icone pour supprimer le favori\" width=\"15\" title=\"Cliquer pour supprimer le favori\">\n</li>")
-  }
+  actualiserFav();
 }
 
 $("#btn-favoris").click(function(){
@@ -19,8 +16,16 @@ $("#btn-favoris").click(function(){
     }else if($.inArray(query, localStorage) == -1){
       console.log("ca marche");
       localStorage.setItem(localStorage.length, query);
+      actualiserFav();
     }else{
       console.log(localStorage);
     }
   })
 })
+
+function actualiserFav(){
+  for(var key in localStorage){
+    $("#liste-favoris").append("<li>\n<span title=\"Cliquer pour relancer la recherche\">"+localStorage.getItem(key)+"</span>\n"
+                                +"<img src=\"images/croix.svg\" alt=\"Icone pour supprimer le favori\" width=\"15\" title=\"Cliquer pour supprimer le favori\">\n</li>")
+  }
+}
